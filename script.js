47dd5fcf46bb81f068ffb9079eb3380e561d6bdb
Peter Sigurdson <peter@petersigurdson.net>
2019-01-14 16:52:48 -0500

    Week 2 - Class Hand In Work

---
+++ script.js
@@ -0,0 +1,14 @@
+// Name: Peter Sigurdon
+// Date: January 14, 2019
+// Student: C0949393
+// Course: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
